a1190d3bb989f482c07c6ac754725c72fd00a45a
PatrikBartos <bartospatrik87@gmail.com>
2025-09-06 18:04:56 +0300

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,7 @@ const sendEmail = async (options) => {
   });
 
   const mailOptions = {
-    from: 'Patrik Bartos <bartospatrik87@gmail.com>',
+    from: 'Max Musterman <Max@gmail.com>',
     to: options.email,
     subject: options.subject,
     text: options.message,
